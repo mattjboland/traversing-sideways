df2573ff03b00173051d056f3b153ed47f9aa187
mattjboland <mattjamesboland@gmail.com>
2020-02-04 18:10:04 +0000

    added img folder and css folder/style.css

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
-  
-$("button").click(function() {
-	
+$(document).ready(function() {
+	//sets <a> element within paragraph  to yellow  
+	$("p").click(function(){
+		$(this).children("a").css("background-color", "yellow"); /* returns all the <a> child elements that are 
+		within this paragraph*/
+	});
+
+	// Toggle the visibility of the paragraph when a button is clicked 
+	$("button").click(function(){
+		$(this).next().slideToggle('slow');
+	});
 });
