9dfc5d8961019b761472078e6e788b2a6aaba072
mattjboland <mattjamesboland@gmail.com>
2020-02-04 18:43:47 +0000

    added all, select_btn index.html and function hide/show script.js

--- js/script.js
+++ js/script.js
@@ -16,5 +16,13 @@ $(document).ready(function() {
     
     $(".card").click(function() {
 		$(this).toggleClass("highlight");	 
+    });
+    
+    $("#select_btn").click(function() {
+		$(".card:not(.highlight)").hide();	 
+    });
+    
+    $("#all_btn").click(function(){
+		$(".card").show();	 
 	});
 });
